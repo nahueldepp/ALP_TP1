dad2f1eb309b42f840a0a4602a5a3c87f3242d01
nahueldepp <nahueldeppen45@gmail.com>
2026-09-18 14:15:00 +0000

    termino el parser

--- src/Parser.hs
+++ src/Parser.hs
@@ -48,8 +48,8 @@ lis = makeTokenParser
 -----------------------------------
 --rehago la grmatica para dar jerarquía
 {-
-intexp :: = interm  
-intterm :: = factor + factor | factor - factor 
+intexp :: = interm  | intterm + intterm | intterm - intterm
+intterm :: =  factor * factor | factor / factor|
 factor :: = (intexp) | nat  | var++ | var--| var | -var
             
 -}
@@ -57,35 +57,32 @@ addop :: Parser (Exp Int -> Exp Int -> Exp Int)
 addop = (reservedOp lis "+" >> return Plus)
   <|> (reservedOp lis "-" >> return Minus)
 
--- No se que hacer con el minus U, preguntar
+
 mulop :: Parser (Exp Int -> Exp Int -> Exp Int)
 mulop = (reservedOp lis "*" >> return Times)
   <|> (reservedOp lis "/" >> return Div)
 
---esto no sé si esta bien, preguntar
+
 varop :: Parser (Exp Int)
 varop = do 
           v <- identifier lis 
-          ((try(reservedOp lis "++") >> return (VarInc v))
-            <|> (try(reservedOp lis "--") >> return (VarDec v)))
-          (return (Var v))
+          (try(reservedOp lis "++") >> return (VarInc v))
+            <|> (try(reservedOp lis "--") >> return (VarDec v))
+              <|> return (Var v)
+            
 
-{-chainl1 p op parses one or more occurrences of p, 
-separated by op Returns a value obtained by a left associative application of all functions returned by op to the values returned by p. 
-This parser can for example be used to eliminate left recursion which typically occurs in expression grammars.
-https://hackage.haskell.org/package/parsec-3.1.18.0/docs/Text-Parsec.html#g:1-}
 intexp :: Parser (Exp Int)
 intexp = chainl1 intterm addop
 
 
 intterm :: Parser (Exp Int)
-intterm = chainl1 factor  mulop
+intterm = chainl1 intfactor  mulop
 
-factor  :: Parser (Exp Int)
-factor  = (parens lis intexp)
+intfactor  :: Parser (Exp Int)
+intfactor  = (parens lis intexp)
   <|> (do 
               reservedOp lis "-"
-              e <- factor
+              e <- intfactor
               return (UMinus e))
     <|> try (do
                 n <- natural lis
@@ -96,15 +93,81 @@ factor  = (parens lis intexp)
 --- Parser de expresiones booleanas
 ------------------------------------
 
+--modifico la gramatica
+{-
+boolexp   :: = !boolterm | boolterm && boolterm | boolterm || boolterm
+boolterm  :: = boolfactor | boolfactor == boolfactor | boolfactor != boolfactor 
+              |boolfactor < boolfactor | boolfactor > boolfacor 
+boolfactor :: = true | false  | (boolexpr)
+-}  
+
+
+
+relop :: Parser (Exp Int -> Exp Int -> Exp Bool)
+relop =
+        (reservedOp lis "==" >> return Eq)
+        <|> (reservedOp lis "!=" >> return NEq)
+        <|> (reservedOp lis ">"  >> return Gt)
+        <|> (reservedOp lis "<"  >> return Lt)
+
+orop :: Parser (Exp Bool -> Exp Bool -> Exp Bool)
+orop = (reservedOp  lis "||" >> (return Or))
+
+andop :: Parser (Exp Bool -> Exp Bool -> Exp Bool) 
+andop = (reservedOp lis "&&"  >> (return And))
+
+comparison :: Parser (Exp Bool)
+comparison = do
+                e1 <- intexp
+                op <- relop
+                e2 <- intexp
+                return (op e1 e2)
+
+
 boolexp :: Parser (Exp Bool)
-boolexp = undefined
+boolexp =   chainl1 boolterm orop
+
+
+boolterm :: Parser (Exp Bool)
+boolterm =  chainl1 boolfactor andop
+
+boolfactor :: Parser (Exp Bool)
+boolfactor =  (parens lis boolexp)
+              <|> try(do
+                  reservedOp lis "!"
+                  b <- boolexp
+                  return (Not b))
+              <|> comparison
+              <|> (reserved lis "true" >> (return BTrue))
+              <|> (reserved lis "false" >> (return BFalse))
+              
 
 -----------------------------------
 --- Parser de comandos
 -----------------------------------
 
 comm :: Parser Comm
-comm = chainl1 
+comm =  (reserved lis "skip" >> (return Skip))
+        <|> try(do 
+                  v <- identifier lis 
+                  reserved lis "=" 
+                  iexp <- intexp 
+                  return (Let v iexp))
+          <|> try(do
+                    reserved  lis "if"
+                    b <- boolexp
+                    com1 <- braces lis comm
+                    reserved lis "else"
+                    com2 <- braces lis comm
+                    return (IfThenElse b com1 com2) 
+                    )
+            <|> try(do
+                      reserved lis "repeat" 
+                      com <- braces lis comm
+                      reserved lis "until"
+                      b <- boolexp
+                      (return (RepeatUntil com b)))
+                        
 
 
 ------------------------------------
