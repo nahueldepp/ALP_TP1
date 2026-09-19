fdcf2ca094f7ea0b060de2386a18bbcad32aef3f
JuanCruzMartelli <jcruzmartelli@gmail.com>
2026-09-18 23:31:24 -0300

    eval3

--- src/Eval3.hs
+++ src/Eval3.hs
@@ -14,22 +14,25 @@ type State = (M.Map Variable Int, String)
 -- Estado vacío
 -- Completar la definición
 initState :: State
-initState = undefined
+initState = (M.empty, [])
 
 -- Busca el valor de una variable en un estado
 -- Completar la definición
 lookfor :: Variable -> State -> Either Error Int
-lookfor = undefined
+lookfor v (m, _) = case M.lookup v m of
+  Nothing -> Left UndefVar
+  Just n -> Right n
 
 -- Cambia el valor de una variable en un estado
 -- Completar la definición
 update :: Variable -> Int -> State -> State
-update = undefined
+update v x (m, t) = (M.insert v x m, t)
 
 -- Agrega una traza dada al estado
 -- Completar la definición
 addTrace :: String -> State -> State
-addTrace = undefined
+addTrace s (m, []) = (m, s)
+addTrace s (m, t) = (m, (t ++ " ") ++ s)
 
 -- Evalúa un programa en el estado vacío
 eval :: Comm -> Either Error State
@@ -46,9 +49,84 @@ stepCommStar c    s = do
 -- Evalúa un paso de un comando en un estado dado
 -- Completar la definición
 stepComm :: Comm -> State -> Either Error (Pair Comm State)
-stepComm = undefined
+stepComm Skip state = return (Skip :!: state)
+stepComm (Let x e) state = do
+  (e' :!: state') <- evalExp e state
+  let msg = "Let " ++ x ++ " " ++ (show e')
+  return (Skip :!: update x e' (addTrace msg state'))
+stepComm (Seq c1 c2) state = case c1 of
+  Skip -> return (c2 :!: state)
+  _ -> do
+    (c1' :!: state') <- stepComm c1 state
+    return ((Seq c1' c2) :!: state')
+stepComm (IfThenElse b c1 c2) state = do
+  (b' :!: state') <- evalExp b state
+  case b' of
+    True -> return (c1 :!: state')
+    False -> return (c2 :!: state')
+stepComm (RepeatUntil c b) state =
+  return ((Seq c (IfThenElse b Skip (RepeatUntil c b))) :!: state)
 
 -- Evalúa una expresión
 -- Completar la definición
 evalExp :: Exp a -> State -> Either Error (Pair a State)
-evalExp = undefined
+evalExp (Const i) state = return (i :!: state)
+evalExp (Var x) state = do
+  n <- lookfor x state
+  return (n :!: state)
+evalExp (UMinus x) state = do
+  (n :!: state') <- evalExp x state
+  return ((-n) :!: state')
+evalExp (Plus a b) state = do
+  (a' :!: state') <- evalExp a state
+  (b' :!: state'') <- evalExp b state'
+  return ((a' + b') :!: state'')
+evalExp (Minus a b) state = do
+  (a' :!: state') <- evalExp a state
+  (b' :!: state'') <- evalExp b state'
+  return ((a' - b') :!: state'')
+evalExp (Times a b) state = do
+  (a' :!: state') <- evalExp a state
+  (b' :!: state'') <- evalExp b state'
+  return ((a' * b') :!: state'')
+evalExp (Div a b) state = do
+  (a' :!: state') <- evalExp a state
+  (b' :!: state'') <- evalExp b state'
+  if b' == 0 then Left DivByZero else return ((a' `div` b') :!: state'')
+evalExp (Eq a b) state = do
+  (a' :!: state') <- evalExp a state
+  (b' :!: state'') <- evalExp b state'
+  return ((a' == b') :!: state'')
+evalExp (Lt a b) state = do
+  (a' :!: state') <- evalExp a state
+  (b' :!: state'') <- evalExp b state'
+  return ((a' < b') :!: state'')
+evalExp (Gt a b) state = do
+  (a' :!: state') <- evalExp a state
+  (b' :!: state'') <- evalExp b state'
+  return ((a' > b') :!: state'')
+evalExp (NEq a b) state = do
+  (a' :!: state') <- evalExp a state
+  (b' :!: state'') <- evalExp b state'
+  return ((a' /= b') :!: state'')
+evalExp BTrue state = return (True :!: state)
+evalExp BFalse state = return (False :!: state)
+evalExp (Not b) state = do
+  (b' :!: state') <- evalExp b state
+  return ((not b') :!: state')
+evalExp (Or a b) state = do
+  (a' :!: state') <- evalExp a state
+  (b' :!: state'') <- evalExp b state'
+  return ((a' || b') :!: state'')
+evalExp (And a b) state = do
+  (a' :!: state') <- evalExp a state
+  (b' :!: state'') <- evalExp b state'
+  return ((a' && b') :!: state'')
+evalExp (VarInc x) state = do
+  v <- lookfor x state
+  let nx = v + 1
+  return (nx :!: (update x nx state))
+evalExp (VarDec x) state = do
+  v <- lookfor x state
+  let nx = v - 1
+  return (nx :!: (update x nx state))
